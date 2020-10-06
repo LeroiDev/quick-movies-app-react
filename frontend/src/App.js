03e06cd53f319f3e3b021cdba7e3b4d7df66ff53
Jen-kins <60917179+Jen-kins@users.noreply.github.com>
2020-10-07 01:38:19 +0200

    movies-useeffect-axios-and-images-request-done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Movie from "./components/Movie";
 
-function App() {
+// api here for now
+// const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.MOVIE_APP_API_KEY}&page=1`;
+
+// const API_IMAGES = "https://image.tmdb.org/t/p.w1280";
+
+// const SEARCH_API = `https"//api.themoviedb.org/3/search/movie?&api_key=${process.env.MOVIE_APP_API_KEY}&query=`;
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`
+      https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_MOVIE_APP_API_KEY}&page=1`);
+      setMovies(response.data.results);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Simple Movies App</h1>
+      {movies.map((movie) => (
+        <Movie key={movie.id} {...movie} />
+      ))}
+      ;
     </div>
   );
-}
+};
 
 export default App;
