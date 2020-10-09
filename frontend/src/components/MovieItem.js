311dc71531521afd9624eb98afcac9c1449c7d68
Jen-kins <60917179+Jen-kins@users.noreply.github.com>
2020-10-09 13:52:59 +0200

    added-no-image-left-on-prop-drilling

--- frontend/src/components/MovieItem.js
+++ frontend/src/components/MovieItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../index.css";
+import noImage  from '../images/no-image-available.jpg'
 const MovieItem = ({ title, poster_path, overview, vote_average }) => {
   const movieRating=(rating)=>{
-if(rating >= 7.5){
+if(rating >= 7){
   return 'green'
-}else if( rating > 5 && rating <7.5)
+}else if( rating > 5 && rating <7)
 return 'orange'
 else{
   return 'red'
@@ -14,7 +15,8 @@ else{
 
   return (
     <div className="movie">
-      <img src={`https://image.tmdb.org/t/p/w1280${poster_path}`} alt={title} />
+      {poster_path ? (<img  src={`https://image.tmdb.org/t/p/w1280${poster_path}`} alt={title}/>) : (<img src={noImage} alt={title}/>) }
+      
       <div className="movie-info">
         <span>{title}</span>
         <span className={`rating-tag ${movieRating(vote_average)}`} >{vote_average}</span>
